b89aa654f7c2ae44822429cf75bc4d0623695688
YJ <rhdtls99@gmail.com>
2024-08-18 01:34:15 +0900

    first commit

---
+++ calculator.js
@@ -0,0 +1,11 @@
+let elcm = document.getElementById('cm');
+let elinch = document.getElementById('inch');
+
+let elCelsius = document.getElementById('Ce');
+let elFahrengeit = document.getElementById('Fa');
+
+let elsubmit = document.getElementById('submit');
+
+function calculateinch() {
+
+}
